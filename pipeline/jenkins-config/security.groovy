1388e196a1dcf445870293ef5993f572cc04e727
Priyanshu Kumar <priyanshukumarcoc10@gmail.com>
2025-07-22 21:56:15 +0530

    Update security.groovy

--- pipeline/jenkins-config/security.groovy
+++ pipeline/jenkins-config/security.groovy
@@ -1,1 +1,18 @@
-// Jenkins security configuration script
+// Jenkins Groovy script for basic security setup
+
+import jenkins.model.*
+import hudson.security.*
+
+def instance = Jenkins.getInstance()
+
+// Create local admin user
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount("admin", "admin123")  // Change in production!
+instance.setSecurityRealm(hudsonRealm)
+
+// Configure authorization strategy
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+strategy.setAllowAnonymousRead(false)
+instance.setAuthorizationStrategy(strategy)
+
+instance.save()
